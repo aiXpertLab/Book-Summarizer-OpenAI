edae88013dc1bcb503da0b048a4d65121ad497d0
aiXpertLab <aiXpertLab@gmail.com>
2024-04-04 08:58:54 -0400

    pypdf2

--- pages/1_📊1_Load_PDF.py
+++ pages/1_📊1_Load_PDF.py
@@ -1,12 +1,12 @@
 import streamlit as st
 from utils.st_def import st_logo, st_load_book
+import openai, PyPDF2, os, time, pandas as pd
 
 st_logo(title='Welcome 👋 to Book Summarizer!', page_title="PDF Summarizer",)
 st_load_book()
 
 pdf1 = st.file_uploader('Upload your PDF Document', type='pdf')
 #-----------------------------------------------
-import openai, PyPDF2, os, time, pandas as pd
 if pdf1:
     pdfReader = PyPDF2.PdfReader(pdf1)
     if 'pdfreader' not in st.session_state:   st.session_state['pdfreader'] = pdfReader
